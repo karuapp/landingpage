4d2e70ba7b2c63453737fbe4ed909f51a12928fe
karuapp <104691902+karuapp@users.noreply.github.com>
2025-07-18 11:27:56 -0600

    cd

--- js/script2.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to display bubble after 2 seconds
-setTimeout(function() {
-    document.getElementById('whatsapp-bubble').style.display = 'block';
-
-    // Trigger typing animation after delay
-    setTimeout(function() {
-        document.getElementById('typing-animation').innerHTML = 'Necesitas ayuda? Contáctanos';
-    }, 500); // Delay for typing animation to start after 500ms
-}, 2000); // Display bubble after 2 seconds
